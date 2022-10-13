630b31d85e2e4d8aa0ebc79763f68e40b697dca8
Luis Gerhorst <gerhorst@cs.fau.de>
2022-10-13 19:05:53 +0200

    bpf: tests

--- tests/call-mem-multi.bpf.s
+++ tests/call-mem-multi.bpf.s
@@ -5,15 +5,24 @@
 ;; invariant that otherwise ensures the branch is never taken.
 .text
     r6 = var ll
+    *(u64 *)(r6 + 0) = 0
     *(u64 *)(r6 + 8) = 0
     r1 = r6
     call 1
-    ;; var may have changed because it was passed to the call, r8 can be != 0 now
-    r8 = *(u64 *)(r6 + 8)
-    ;; May seem as if r8==7 is guaranteed here. Then branch is never taken.
+    r8 = *(u64 *)(r6 + 0)
+    r9 = *(u64 *)(r6 + 8)
+    ;; wrong: r8==0 ^ r9==0 <=>
+    ;;  <=> (r8==0 & r9!=0 | r8!=0 & r9==0)
+    ;;  <=> gadget always reached
+    ;;  <=> safe
+    ;; possible: r8!=0 & r9!=0
+    if r8 == 0 goto Gadget
+    ;; r8!=0
+    if r9 != 0 goto End
+    ;; r8!=0 & r9==0
     ;;
-    ;; Gadget, unless never taken:
-    if r8 > 0 goto End
+    ;; Safe, if always reached.
+Gadget:
     r2 = *(u64 *)(r2 + 0)
     r2 *= 512
     r2 = *(u64 *)(r2 + 0)

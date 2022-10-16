48acde227ca2802611ca7062f47b9025a0dc40df
Luis Gerhorst <gerhorst@cs.fau.de>
2022-10-16 16:41:57 +0200

    tests/bpf += reg,regs

--- tests/reg.bpf.s
+++ tests/reg.bpf.s
@@ -2,22 +2,10 @@
 ;;
 ;; Gadget should never execute regularly, speculation leaks data.
 .text
-    r2 = *(u64 *)(r1 + 0)
-    r8 = 1
-    r9 = 1
-    ;; wrong: r8==0 ^ r9==0 <=>
-    ;;  <=> (r8==0 & r9!=0 | r8!=0 & r9==0)
-    ;;  <=> gadget always reached
-    ;;  <=> safe
-    ;; possible: r8!=0 & r9!=0
-    if r8 == 0 goto Gadget
-    ;; r8!=0
-    if r9 != 0 goto End
-    ;; r8!=0 & r9==0
-    ;;
-    ;; Safe, if always reached.
+    *(u64 *)(r1 + 0) = 0
+    r9 = *(u64 *)(r1 + 0)
+    if r9 == 0 goto End
 Gadget:
-    ;; r8==0 | r9==0
     r2 = *(u64 *)(r2 + 0)
     r2 *= 512
     r2 = *(u64 *)(r2 + 0)

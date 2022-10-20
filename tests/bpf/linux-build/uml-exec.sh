95e320290b3392e5debc818d31584624a5be8c67
Luis Gerhorst <gerhorst@cs.fau.de>
2022-10-20 11:07:53 +0200

    uml scripts

---
+++ tests/bpf/linux-build/uml-exec.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+CMD=$1
+DIR=$2
+
+# TODO
--- tests/bpf/linux-build/uml-install-root.sh
+++ tests/bpf/linux-build/uml-install-root.sh
@@ -3,29 +3,44 @@ set -euo pipefail
 
 DISK=$1
 MNT=$2
+HOSTNAME=$3
+DEBOOTSTRAP_CACHE_DIR=$4
 
-debootstrap bullseye $MNT http://deb.debian.org/debian
+debootstrap --cache-dir=$DEBOOTSTRAP_CACHE_DIR bullseye $MNT http://deb.debian.org/debian
 
 # Create an fstab file
-echo "/dev/ubd0  /       ext4     defaults     0 1" > $MNT/etc/fstab
-echo "proc       /proc   proc    defaults     0 0" >> $MNT/etc/fstab
+echo "/dev/ubd0 / ext4 defaults 0 1" > $MNT/etc/fstab
+echo "proc /proc proc defaults 0 0" >> $MNT/etc/fstab
+echo "sysfs /sys sysfs defaults 0 0" >> $MNT/etc/fstab
+
+# Based on Ubuntu 22:
+echo "127.0.0.1	localhost
+127.0.1.1	$HOSTNAME
+
+# The following lines are desirable for IPv6 capable hosts
+::1     ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+" > $MNT/etc/hosts
 
 # Create the device file for the root file system and configure permissions
 mknod --mode=660 $MNT/dev/ubd0 b 98 0
 chown root:disk $MNT/dev/ubd0
 
 # Create a hosts file
-echo "127.0.0.1 localhost" > $MNT/etc/hosts
+# echo "127.0.0.1 localhost" > $MNT/etc/hosts
 
 # Configure network interfaces: eth0 using dhcp
-echo "auto lo" > $MNT/etc/network/interfaces
-echo "iface lo inet loopback" >> $MNT/etc/network/interfaces
-echo "auto eth0" >> $MNT/etc/network/interfaces
-echo "iface eth0 inet dhcp" >> $MNT/etc/network/interfaces
+# echo "auto lo" > $MNT/etc/network/interfaces
+# echo "iface lo inet loopback" >> $MNT/etc/network/interfaces
+# echo "auto eth0" >> $MNT/etc/network/interfaces
+# echo "iface eth0 inet dhcp" >> $MNT/etc/network/interfaces
 
 # Add first terminal and serial terminal to secure list
-echo "tty0" >> $MNT/etc/securetty
-echo "ttys/0" >> $MNT/etc/securetty
+# echo "tty0" >> $MNT/etc/securetty
+# echo "ttys/0" >> $MNT/etc/securetty
 
 # Remove the other terminal events
 # rm $MNT/etc/event.d/tty2

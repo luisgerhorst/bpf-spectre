21737bd79af4ba064b7495681b8379d63055ec31
Luis Gerhorst <gerhorst@cs.fau.de>
2022-10-20 16:36:26 +0200

    ...

--- tests/bpf/linux-build/env.sh
+++ tests/bpf/linux-build/env.sh
@@ -1,1 +1,7 @@
-export BZIMAGE=linux/arch/x86_64/boot/bzImage
+export TARGET_SSH="ssh ${SSH_DEST} -p ${SSH_PORT} -o BatchMode=true -o NoHostAuthenticationForLocalhost=true"
+export TARGET_SCP="scp -P ${SSH_PORT} -B -o NoHostAuthenticationForLocalhost=true -q"
+
+export TARGET_EXEC="./scripts/target-exec ${SSH_DEST} ${SSH_PORT}"
+
+export TARGET_PREFIX='~/tmp/bpftask'
+export TARGET_PERF='./linux-perf/perf-*/tools/perf/perf'

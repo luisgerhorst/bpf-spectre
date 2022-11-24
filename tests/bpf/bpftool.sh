5e500637ac4268fa3995159158a75fe0d2eb44c6
Luis Gerhorst <gerhorst@cs.fau.de>
2022-11-24 15:47:19 +0100

    bpftool: log verifier trace

--- tests/bpf/bpftool.sh
+++ tests/bpf/bpftool.sh
@@ -13,7 +13,7 @@ bpftool --version
 $cs 'capsh --print'
 
 path=/sys/fs/bpf/$(basename $obj .bpf.o)
-$cs "bpftool prog loadall $obj $path" 2> ../result_dir/loadall.log
+$cs "bpftool --debug prog loadall $obj $path" 2> ../result_dir/loadall.log
 exitcode=$?
 echo $exitcode > ../result_dir/loadall.bpftool-guess.exitcode
 echo "NA" > ../result_dir/loadall.inferred-type
@@ -23,7 +23,7 @@ then
     for type in socket sk_reuseport/migrate sk_reuseport kprobe+ uprobe+ uprobe.s+ kretprobe+ uretprobe+ uretprobe.s+ kprobe.multi+ kretprobe.multi+ ksyscall+ kretsyscall+ usdt+ tc classifier action tracepoint+ tp+ raw_tracepoint+ raw_tp+ raw_tracepoint.w+ raw_tp.w+ tp_btf+ fentry+ fmod_ret+ fexit+ fentry.s+ fmod_ret.s+ fexit.s+ freplace+ lsm+ lsm.s+ lsm_cgroup+ iter+ iter.s+ syscall xdp.frags/devmap xdp/devmap xdp.frags/cpumap xdp/cpumap xdp.frags xdp perf_event lwt_in lwt_out lwt_xmit lwt_seg6local sockops sk_skb/stream_parser sk_skb/stream_verdict sk_skb sk_msg lirc_mode2 flow_dissector cgroup_skb/ingress cgroup_skb/egress cgroup/skb cgroup/sock_create cgroup/sock_release cgroup/sock cgroup/post_bind4 cgroup/post_bind6 cgroup/bind4 cgroup/bind6 cgroup/connect4 cgroup/connect6 cgroup/sendmsg4 cgroup/sendmsg6 cgroup/recvmsg4 cgroup/recvmsg6 cgroup/getpeername4 cgroup/getpeername6 cgroup/getsockname4 cgroup/getsockname6 cgroup/sysctl cgroup/getsockopt cgroup/setsockopt cgroup/dev struct_ops+ sk_lookup
     do
         type_path=$(echo $type | tr '/+.' "___")
-        $cs "bpftool prog loadall $obj $path type $type" 2>> ../result_dir/loadall.type.$type_path.log
+        $cs "bpftool --debug prog loadall $obj $path type $type" 2>> ../result_dir/loadall.type.$type_path.log
         exitcode=$?
         echo $exitcode > ../result_dir/loadall.$type_path.exitcode
         if [ $exitcode == "0" ]
@@ -41,5 +41,9 @@ do
     bpftool prog dump xlated pinned "$prog" > ../result_dir/xlated.$(basename $prog)
     bpftool prog dump jited pinned "$prog" > ../result_dir/jited.$(basename $prog)
 done
+
+# TODO:
+# bpftool prog run PROG data_in FILE
+# bpftool prog profile PROG [duration DURATION] METRICs
 
 rm -rfd $path

557a04b49f3e5fe9c8774fca5c1bca883c9069ff
Luis Gerhorst <gerhorst@cs.fau.de>
2023-03-15 16:13:17 +0100

    localversion based on git diff / defconfig

--- src/scripts/linux-localversion.sh
+++ src/scripts/linux-localversion.sh
@@ -5,13 +5,21 @@
     set -x
 
     LINUX=$1
+    MAKE=${MAKE:-make}
 
     # Do this here and not in system's Makefile because the Makefile may change linux/.config.
-    CONFIG=$LINUX/.config
+
+    $MAKE -C $LINUX savedefconfig > /dev/null
+
     KERNEL_COMMIT=$(env -C ${LINUX} git rev-parse --short HEAD | cut --characters=1-6)
-    KERNEL_STATUS_MD5SUM=$(env -C $LINUX git status -vv | awk '$1 != "index" { print }' | md5sum)
-    KERNEL_CONFIG_MD5SUM=$(md5sum $CONFIG)
-    LOCALVERSION=-$USER+$KERNEL_COMMIT+$(echo $KERNEL_STATUS_MD5SUM+$KERNEL_CONFIG_MD5SUM | md5sum | cut --characters=1-6)
+    diff="$(env -C $LINUX git diff)"
+    KERNEL_STATUS_MD5SUM="+$(echo "$diff" | md5sum | cut --characters=1-6)"
+    if test "$diff" = ""
+    then
+        KERNEL_STATUS_MD5SUM=""
+    fi
+    KERNEL_CONFIG_MD5SUM=$(cat $LINUX/defconfig | md5sum | cut --characters=1-6)
+    LOCALVERSION=-$USER+$KERNEL_COMMIT+$KERNEL_CONFIG_MD5SUM$KERNEL_STATUS_MD5SUM
 
     echo "$LOCALVERSION"
     exit
